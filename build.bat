86843c9e57f89bfc9138f957a3de2a7505e91ed0
tamirre <100494736+tamirre@users.noreply.github.com>
2024-08-04 00:23:49 +0200

    Updated vscode settings, build.bat and tutorial code

--- build.bat
+++ build.bat
@@ -2,11 +2,10 @@
 
 rem SET PATH=%PATH%;%VULKAN_SDK%/Include
 rem echo %PATH%
-SET includes=/Isrc /I%VULKAN_SDK%/Include
-SET links=/link /LIBPATH:%VULKAN_SDK%/Lib vulkan-1.lib
-rem user32.lib Gdi32.lib
-SET defines=/D INTERNAL=1 /D DEBUG=1 /FC /Zi /Fo"..\\build\\" /Fe:.."\\bin\\game.exe" 
-SET src=%~dp0\src\main.cpp
+SET includes=/Isrc /I%VULKAN_SDK%/Include 
+SET links=/link /LIBPATH:%VULKAN_SDK%/Lib vulkan-1.lib User32.lib
+SET defines=/D DEBUG=1 /Fo"..\\..\\build\\" /Fe:"..\\..\\bin\\game.exe" 
+SET src=%~dp0\src\platform\win32_platform.cpp
 
 SET out=%~dp0\build\
 SET bin=%~dp0\bin\
@@ -15,5 +14,5 @@ IF NOT EXIST %out% mkdir %out%
 IF NOT EXIST %bin% mkdir %bin%
 
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
-cl /EHsc %includes% %defines% %src% %links% 
+cl /EHsc /Z7 %includes% %defines% %src% %links% 
 
