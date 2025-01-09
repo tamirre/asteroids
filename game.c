b1566b25c796354bcf4bc536247a31a83de2ec3e
tamirre <100494736+tamirre@users.noreply.github.com>
2025-01-09 21:54:08 +0100

    Added parallax star background and fixed collision issues

--- game.c
+++ game.c
@@ -6,8 +6,13 @@
 #define SCREEN_WIDTH (700.0f)
 #define SCREEN_HEIGHT (400.0f)
 #define WINDOW_TITLE ("Asteroids")
-#define MAX_BULLETS (100)
-#define MAX_ENEMIES (100)
+#define MAX_BULLETS (50)
+#define MAX_ENEMIES (20)
+#define MAX_STARS (10)
+// #define PARALLAX_LAYERS (2)
+
+#define MAX(a,b) ((a) > (b) ? (a) : (b))
+#define MIN(a,b) ((a) < (b) ? (a) : (b))
 
 typedef enum State
 {
@@ -16,6 +21,15 @@ typedef enum State
     STATE_GAME_OVER,
     STATE_PAUSED,
 } State;
+
+typedef struct Star {
+    // int arrayIndex;
+    Vector2 position;
+    float size;
+    float velocity;
+    int imgIndex;
+    float alpha;
+} Star;
 
 typedef struct Enemy {
     Vector2 position;
@@ -49,6 +63,14 @@ typedef struct GameState {
     int enemyCount;
     float spawnTime;
     float enemySpawnRate;
+    float lastEnemyXPosition;
+    // Parallax background
+    Star starsLayer1[MAX_STARS];
+    Star starsLayer2[MAX_STARS];
+    int starCountLayer1;
+    int starCountLayer2;
+    float starTime;
+    float starSpawnRate;
 } GameState;
 
 static const GameState defaultGameState = {
@@ -61,9 +83,14 @@ static const GameState defaultGameState = {
     .playerVelocity = 200,
     .shootTime = 0.0f,
     .enemyCount = 0,
-    .enemySpawnRate = 0.5f,
+    .enemySpawnRate = 1.0f,
     .spawnTime = 0.0,
     .score = 0,
+    .starCountLayer1 = 0,
+    .starCountLayer2 = 0,
+    .starTime = 0,
+    .starSpawnRate = 0.25f,
+    .lastEnemyXPosition = 0.0f,
 };
 
 void draw_text_centered(const char* text, Vector2 pos, float fontSize, Color color)
@@ -95,24 +122,134 @@ int main() {
         switch (gameState.state) {
             case STATE_MAIN_MENU:
             {
+                Color backgroundColor = ColorFromHSV(259, 1, 0.07);
+                ClearBackground(backgroundColor);
                 draw_text_centered("Asteroids", (Vector2){SCREEN_WIDTH/2.0f, SCREEN_HEIGHT/2.0f}, 40, WHITE);
                 draw_text_centered("<Press enter to play>", (Vector2){SCREEN_WIDTH/2.0f, SCREEN_HEIGHT/2.0f + 30}, 20, WHITE);
                 draw_text_centered("<WASD to move, space to shoot, p to pause>", (Vector2){SCREEN_WIDTH/2.0f, SCREEN_HEIGHT/2.0f + 50}, 20, WHITE);
                 draw_text_centered("v0.1", (Vector2){SCREEN_WIDTH/2.0f, SCREEN_HEIGHT - 15}, 15, WHITE);
                 if (IsKeyPressed(KEY_ENTER)) {                    
                     gameState.state = STATE_RUNNING;
+                    // Spawn initial stars for parallax
+                    {
+                        while(gameState.starCountLayer1 < MAX_STARS)
+                        {
+                            // float size = GetRandomValue(100.0, 300.0) / 100.0f;   
+                            int imgIndex = GetRandomValue(1, 2);   
+                            float starXPosition = GetRandomValue(0, SCREEN_WIDTH); 
+                            float starYPosition = GetRandomValue(0, SCREEN_HEIGHT); 
+                            Star star1 = {
+                                // .arrayIndex = gameState.starCount,
+                                .position = (Vector2) {starXPosition, starYPosition},
+                                .velocity = 50.0,
+                                .size = 1,
+                                .imgIndex = imgIndex,
+                                .alpha = 0.5,
+                            };
+                            gameState.starsLayer1[gameState.starCountLayer1++] = star1;
+                        }
+                        while(gameState.starCountLayer2 < MAX_STARS)
+                        {
+                            int imgIndex = GetRandomValue(1, 2);   
+                            float starXPosition = GetRandomValue(0, SCREEN_WIDTH); 
+                            float starYPosition = GetRandomValue(0, SCREEN_HEIGHT); 
+                            Star star2 = {
+                                // .arrayIndex = gameState.starCount,
+                                .position = (Vector2) {starXPosition, starYPosition},
+                                .velocity = 100.0f,
+                                .size = 1,
+                                .imgIndex = imgIndex,
+                                .alpha = 1.0f,
+                            };
+                            gameState.starsLayer2[gameState.starCountLayer2++] = star2;
+                        }
+                    }
                 }
                 break;
             }
             case STATE_RUNNING:
             {
-                gameState.gameTime += GetFrameTime(); 
+                Color backgroundColor = ColorFromHSV(258, 1, 0.07);
+                ClearBackground(backgroundColor);
+                // Spawn new stars for parallax
+                {
+                    gameState.starTime += GetFrameTime();
+                    if (gameState.starTime > gameState.starSpawnRate) 
+                    {
+                        gameState.starTime = 0;
+                        if (gameState.starCountLayer1 < MAX_STARS)
+                        {
+                            int imgIndex = GetRandomValue(1, 2);   
+                            float starXPosition = GetRandomValue(0, SCREEN_WIDTH); 
+                            Star star1 = {
+                                // .arrayIndex = gameState.starCount,
+                                .position = (Vector2) {starXPosition, 0},
+                                .velocity = 50.0f,
+                                .size = 1,
+                                .imgIndex = imgIndex,
+                                .alpha = 0.5f,
+                            };
+                            gameState.starsLayer1[gameState.starCountLayer1++]= star1;
+                        }
+                        if (gameState.starCountLayer2 < MAX_STARS)
+                        {
+                            int imgIndex = GetRandomValue(1, 2);   
+                            float starXPosition = GetRandomValue(0, SCREEN_WIDTH); 
+                            Star star2 = {
+                                // .arrayIndex = gameState.starCount,
+                                .position = (Vector2) {starXPosition, 0},
+                                .velocity = 100.0f,
+                                .size = 1,
+                                .imgIndex = imgIndex,
+                                .alpha = 1.0f,
+                            };
+                            gameState.starsLayer2[gameState.starCountLayer2++]= star2;    
+                        }
+                    }
+                }
+                // Update Stars
+                {
+                    for (int starIndex = 0; starIndex < gameState.starCountLayer1; starIndex++)
+                    {
+                        Star* star = &gameState.starsLayer1[starIndex];
+                        if(!CheckCollisionPointRec(star->position, screenRect))
+						{
+							// Replace with last star
+							 *star = gameState.starsLayer1[--gameState.starCountLayer1];
+						}
+                        star->position.y += star->velocity * GetFrameTime();
+                        char imgCharBuffer[100] = { 0 };
+                        sprintf(imgCharBuffer, "assets/star%d.png", star->imgIndex);
+                        Texture2D texture = LoadTexture(imgCharBuffer);
+                        const int texture_x = star->position.x - texture.width / 2 * star->size;
+                        const int texture_y = star->position.y - texture.height / 2 * star->size;\
+                        Color starColor = ColorAlpha(WHITE, star->alpha);
+                        DrawTextureEx(texture, (Vector2) {texture_x, texture_y}, 0.0, star->size, starColor);
+                    }
+                    for (int starIndex = 0; starIndex < gameState.starCountLayer2; starIndex++)
+                    {
+                        Star* star = &gameState.starsLayer2[starIndex];
+                        if(!CheckCollisionPointRec(star->position, screenRect))
+						{
+							// Replace with last star
+							 *star = gameState.starsLayer2[--gameState.starCountLayer2];
+						}
+                        star->position.y += star->velocity * GetFrameTime();
+                        char imgCharBuffer[100] = { 0 };
+                        sprintf(imgCharBuffer, "assets/star%d.png", star->imgIndex);
+                        Texture2D texture = LoadTexture(imgCharBuffer);
+                        const int texture_x = star->position.x - texture.width / 2 * star->size;
+                        const int texture_y = star->position.y - texture.height / 2 * star->size;\
+                        Color starColor = ColorAlpha(WHITE, star->alpha);
+                        DrawTextureEx(texture, (Vector2) {texture_x, texture_y}, 0.0, star->size, starColor);
+                    }
+                }
+                
+                // gameState.gameTime += GetFrameTime(); 
                                
                 Texture2D texture = LoadTexture("assets/rocket.png");
                 const int texture_x = gameState.playerPosition.x - texture.width / 2;
                 const int texture_y = gameState.playerPosition.y - texture.height / 2;
-                DrawTextureEx(texture, (Vector2) {texture_x, texture_y}, 0.0, 1.0, WHITE);
-
                 if (IsKeyDown(KEY_W)) {
                     if(!CheckCollisionPointLine(gameState.playerPosition, (Vector2) {0, 0}, (Vector2) {SCREEN_WIDTH, 0}, texture.height / 2))
                     {
@@ -165,7 +302,6 @@ int main() {
                 {
                     for (int bulletIndex = 0; bulletIndex < gameState.bulletCount; bulletIndex++)
                     {
- 
                         Bullet* bullet = &gameState.bullets[bulletIndex];
                         if(!CheckCollisionPointRec(bullet->position, screenRect))
 						{
@@ -180,25 +316,31 @@ int main() {
                     }
                         
                 }
+                // Update Player
+                DrawTextureEx(texture, (Vector2) {texture_x, texture_y}, 0.0, 1.0, WHITE);
                 // Spawn Enemies
                 {
                     gameState.spawnTime += GetFrameTime();
-                    if (gameState.spawnTime > gameState.enemySpawnRate) 
+                    if (gameState.spawnTime > gameState.enemySpawnRate && gameState.enemyCount < MAX_ENEMIES) 
                     {
-                        if (gameState.enemyCount >= MAX_ENEMIES)
-                        {
-                            break;
-                        }
-                        float size = GetRandomValue(100.0, 300.0) / 100.0f;   
-                        float enemyXPosition = GetRandomValue(0, SCREEN_WIDTH); 
+                        // if (gameState.enemyCount >= MAX_ENEMIES)
+                        // {
+                        //     break;
+                        // } 
+                        float size = GetRandomValue(100.0, 300.0) / 100.0f;
+                        float minSpawnDistance = 31.0f * size;  
+                        float enemyXPosition = MAX(minSpawnDistance, GetRandomValue(0, SCREEN_WIDTH)); 
+                        // float enemyXPosition = MAX(gameState.lastEnemyXPosition + minSpawnDistance, GetRandomValue(0, SCREEN_WIDTH)); 
+                        // gameState.lastEnemyXPosition = enemyXPosition;
                         Enemy enemy = {
                             .position = (Vector2) {enemyXPosition, 0},
-                            .health = (int) size * 2,
-                            .velocity = 50.0,
+                            .health = (int) (size+1.0) * 2.0,
+                            .velocity = 35.0,
                             .size = size,
                         };
                         gameState.spawnTime = 0;
                         gameState.enemies[gameState.enemyCount++] = enemy;
+
                     }
                 }
                 // Update Enemies
@@ -236,14 +378,19 @@ int main() {
                             }
                         }
                         Rectangle playerRec = {
-                                .width = 26,
-                                .height = 27,
-                                .x = gameState.playerPosition.x - 26/2,
-                                .y = gameState.playerPosition.y - 27/2,
+                            .width = 26,
+                            .height = 27,
+                            .x = gameState.playerPosition.x - 26/2,
+                            .y = gameState.playerPosition.y - 27/2,
                         };
-
                         // DrawRectangleRec(enemyRec, RED); 
                         // DrawRectangleRec(playerRec, BLUE);
+                        // || enemy->position.y > SCREEN_HEIGHT
+                        if(!CheckCollisionPointRec(enemy->position, screenRect))
+                        {
+                            // Replace with last enemy
+                            *enemy = gameState.enemies[--gameState.enemyCount];
+                        }
                         if(CheckCollisionRecs(enemyRec, playerRec))
                         {
                             *enemy = gameState.enemies[--gameState.enemyCount];
