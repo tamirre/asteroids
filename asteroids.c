2a3eb3970314c867e7871fe6baa92485f6dbc4eb
tamirre <tamir.d@web.de>
2025-04-20 18:32:02 +0200

    Added texture atlas and restructured textures to atlas struct

--- asteroids.c
+++ asteroids.c
@@ -1,8 +1,12 @@
+// To build on linux: 
+// gcc asteroids.c -Wall -o asteroids -Ithird_party/include -lraylib -lm -ldl -lpthread -lGL
 #define RAYMATH_IMPLEMENTATION
 #include "raylib.h"
-#include "raymath.h"
-#include <stdlib.h>
+// #include <stdlib.h>
 #include <stdio.h>
+// #define ASSETS_IMPLEMENTATION
+#include "assets.h"
+
 #define SCREEN_WIDTH (700.0f)
 #define SCREEN_HEIGHT (400.0f)
 #define WINDOW_TITLE ("Asteroids")
@@ -14,14 +18,6 @@
 #define MAX(a,b) ((a) > (b) ? (a) : (b))
 #define MIN(a,b) ((a) < (b) ? (a) : (b))
 
-typedef struct SpriteAnimation
-{
-    Texture2D atlas;
-    int framesPerSecond;
-    float timeStarted;
-    Rectangle* rectangles;
-    int rectanglesLentgh;
-} SpriteAnimation;
 
 typedef enum State
 {
@@ -68,7 +64,6 @@ typedef struct GameState {
     Vector2 playerPosition;
     int playerHealth;
     bool playerMultishot;
-    Texture2D playerTexture;
     SpriteAnimation playerAnimation;
     // Projectiles
     Bullet bullets[MAX_BULLETS];
@@ -110,46 +105,6 @@ static const GameState defaultGameState = {
     .initStars = 0,
 };
 
-SpriteAnimation createSpriteAnimation(Texture2D atlas, int framesPerSecond, Rectangle rectangles[], int length)
-{
-    SpriteAnimation spriteAnimation = 
-    {
-        .atlas = atlas,
-        .framesPerSecond = framesPerSecond,
-        .rectanglesLentgh = length,
-        .rectangles = NULL,
-        .timeStarted = GetTime(),
-    };
-
-    Rectangle* mem = (Rectangle*)malloc(sizeof(Rectangle) * length);
-    if (mem == NULL)
-    {
-        TraceLog(LOG_FATAL, "No memory for CreateSpriteAnimation");
-        spriteAnimation.rectanglesLentgh = 0;
-        return spriteAnimation;
-    }
-    spriteAnimation.rectangles = mem;
-
-    for(int i = 0; i < length; i++)
-    {
-        spriteAnimation.rectangles[i] = rectangles[i];
-    }
-
-    return spriteAnimation;
-}
-
-void DrawSpriteAnimationPro(SpriteAnimation animation, Rectangle destination, Vector2 origin, float rotation, Color tint)
-{
-    int index = (int)(GetTime() * animation.framesPerSecond) % animation.rectanglesLentgh;
-    Rectangle source = animation.rectangles[index];
-    DrawTexturePro(animation.atlas, source, destination, origin, rotation, tint);
-}
-
-void FreeSpriteAnimation(SpriteAnimation animation)
-{
-    free(animation.rectangles);
-}
-
 void draw_text_centered(Font font, const char* text, Vector2 pos, float fontSize, float fontSpacing, Color color)
 {
 	const Vector2 textSize = MeasureTextEx(font, text, fontSize, fontSpacing);
@@ -160,22 +115,24 @@ void draw_text_centered(Font font, const char* text, Vector2 pos, float fontSize
 
 int main() {
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, WINDOW_TITLE);
-    SetTargetFPS(60);
+    SetTargetFPS(240);
     
     Font font = LoadFont("fonts/setback.png");
     int fontSpacing = 1;
     int fontSize = 15;
 
     GameState gameState = defaultGameState;
-    Texture2D playerTexture = LoadTexture("assets/rocketNew.png");
-    gameState.playerTexture = playerTexture;
-    gameState.playerAnimation = createSpriteAnimation(playerTexture, 10, (Rectangle[]) {
-        (Rectangle){0,0,32,62},   (Rectangle){32,0,32,62},  (Rectangle){64,0,32,62},  (Rectangle){96,0,32,62},
-        (Rectangle){128,0,32,62}, (Rectangle){160,0,32,62}, (Rectangle){192,0,32,62}, (Rectangle){224,0,32,62},
-        (Rectangle){256,0,32,62}, (Rectangle){288,0,32,62}, (Rectangle){320,0,32,62}, (Rectangle){352,0,32,62},
-        (Rectangle){384,0,32,62}, (Rectangle){416,0,32,62}, (Rectangle){448,0,32,62}, (Rectangle){480,0,32,62},
-        (Rectangle){512,0,32,62}, (Rectangle){544,0,32,62}, (Rectangle){576,0,32,62}, (Rectangle){608,0,32,62},        
-    }, 20);
+    TextureAtlas atlas;
+    // Texture2D playerTexture = LoadTexture("assets/rocketNew.png");
+    // gameState.playerTexture = playerTexture;
+    // gameState.playerAnimation = createSpriteAnimation(playerTexture, 10, (Rectangle[]) {
+    //     (Rectangle){0,0,32,62},   (Rectangle){32,0,32,62},  (Rectangle){64,0,32,62},  (Rectangle){96,0,32,62},
+    //     (Rectangle){128,0,32,62}, (Rectangle){160,0,32,62}, (Rectangle){192,0,32,62}, (Rectangle){224,0,32,62},
+    //     (Rectangle){256,0,32,62}, (Rectangle){288,0,32,62}, (Rectangle){320,0,32,62}, (Rectangle){352,0,32,62},
+    //     (Rectangle){384,0,32,62}, (Rectangle){416,0,32,62}, (Rectangle){448,0,32,62}, (Rectangle){480,0,32,62},
+    //     (Rectangle){512,0,32,62}, (Rectangle){544,0,32,62}, (Rectangle){576,0,32,62}, (Rectangle){608,0,32,62},        
+    // }, 20);
+    initTextureAtlas(&atlas);
 
     while (!WindowShouldClose())
     {
@@ -219,11 +176,20 @@ int main() {
                         while(gameState.starCount < MAX_STARS)
                         {                            
                             int imgIndex = GetRandomValue(1, 2);   
-                            char imgCharBuffer[100] = { 0 };
+                            // char imgCharBuffer[100] = { 0 };
                             float starXPosition = GetRandomValue(0, SCREEN_WIDTH); 
                             float starYPosition = GetRandomValue(0, SCREEN_HEIGHT); 
-                            sprintf(imgCharBuffer, "assets/star%d.png", imgIndex);
-                            Texture2D texture = LoadTexture(imgCharBuffer);
+                            // sprintf(imgCharBuffer, "assets/star%d.png", imgIndex);
+                            Texture2D texture;
+                            if (imgIndex == 1)
+                            {
+                                texture = atlas.starTexture1;
+                            } 
+                            else
+                            {
+                                texture = atlas.starTexture2;
+                            }
+
                             Star star = {
                                 .position = (Vector2) {starXPosition, starYPosition},
                                 .velocity = 30.0f * GetRandomValue(1,2) * imgIndex,
@@ -245,10 +211,18 @@ int main() {
                         if (gameState.starCount < MAX_STARS)
                         {
                             int imgIndex = GetRandomValue(1, 2);  
-                            char imgCharBuffer[100] = { 0 }; 
+                            // char imgCharBuffer[100] = { 0 }; 
+                            Texture2D texture;
+                            if (imgIndex == 1)
+                            {
+                                texture = atlas.starTexture1;
+                            } 
+                            else if (imgIndex == 2)
+                            {
+                                texture = atlas.starTexture2;
+                            }
                             float starXPosition = GetRandomValue(0, SCREEN_WIDTH); 
-                            sprintf(imgCharBuffer, "assets/star%d.png", imgIndex);
-                            Texture2D texture = LoadTexture(imgCharBuffer);
+                            // sprintf(imgCharBuffer, "assets/star%d.png", imgIndex);
                             Star star = {
                                 .position = (Vector2) {starXPosition, 0},
                                 .velocity = 30.0f * GetRandomValue(1,2) * imgIndex,
@@ -271,7 +245,7 @@ int main() {
 							*star = gameState.stars[--gameState.starCount];
 						}
                         star->position.y += star->velocity * GetFrameTime();
-                        char imgCharBuffer[100] = { 0 };
+                        // char imgCharBuffer[100] = { 0 };
                         
                         const int texture_x = star->position.x - star->texture.width / 2.0 * star->size;
                         const int texture_y = star->position.y - star->texture.height / 2.0 * star->size;
@@ -328,33 +302,33 @@ int main() {
                             .position = gameState.playerPosition,
                             .velocity = 100.0f,
                             .damage = 1.0,
-                            .texture = LoadTexture("assets/bullet.png"),
+                            .texture = atlas.bulletTexture,
                         };
                         gameState.bullets[gameState.bulletCount++] = bullet1;
                         float texture_x = gameState.playerPosition.x - bullet1.texture.width / 2.0;
-                        float texture_y = gameState.playerPosition.y - gameState.playerTexture.height / 2.0 - bullet1.texture.height / 2.0;
+                        float texture_y = gameState.playerPosition.y - atlas.playerTexture.height / 2.0 - bullet1.texture.height / 2.0;
                         DrawTexture(bullet1.texture, texture_x, texture_y, WHITE);
                         Bullet bullet2 = 
                         {
                             .position = (Vector2){gameState.playerPosition.x - 40.0f, gameState.playerPosition.y + 0.0f},
                             .velocity = 100.0f,
                             .damage = 1.0,
-                            .texture = LoadTexture("assets/bullet.png"),
+                            .texture = atlas.bulletTexture,
                         };
                         gameState.bullets[gameState.bulletCount++] = bullet2;
                         texture_x = gameState.playerPosition.x - 40.0f - bullet2.texture.width / 2.0;
-                        texture_y = gameState.playerPosition.y - gameState.playerTexture.height / 2.0 - bullet2.texture.height / 2.0;
+                        texture_y = gameState.playerPosition.y - atlas.playerTexture.height / 2.0 - bullet2.texture.height / 2.0;
                         DrawTexture(bullet2.texture, texture_x, texture_y, WHITE);
                         Bullet bullet3 = 
                         {
                             .position = (Vector2){gameState.playerPosition.x + 40.0f, gameState.playerPosition.y + 0.0f},
                             .velocity = 100.0f,
                             .damage = 1.0,
-                            .texture = LoadTexture("assets/bullet.png"),
+                            .texture = atlas.bulletTexture,
                         };
                         gameState.bullets[gameState.bulletCount++] = bullet3;
                         texture_x = gameState.playerPosition.x + 40.0f - bullet3.texture.width / 2.0;
-                        texture_y = gameState.playerPosition.y - gameState.playerTexture.height / 2.0 - bullet3.texture.height / 2.0;
+                        texture_y = gameState.playerPosition.y - atlas.playerTexture.height / 2.0 - bullet3.texture.height / 2.0;
                         DrawTexture(bullet3.texture, texture_x, texture_y, WHITE);
                         gameState.shootTime -= gameState.shootDelay;
                     }
@@ -365,12 +339,12 @@ int main() {
                                 .position = gameState.playerPosition,
                                 .velocity = 100.0f,
                                 .damage = 1.0,
-                                .texture = LoadTexture("assets/bullet.png"),
+                                .texture = atlas.bulletTexture,
                             };
                         gameState.bullets[gameState.bulletCount++] = bullet;
                         gameState.shootTime -= gameState.shootDelay;
                         const int texture_x = gameState.playerPosition.x - bullet.texture.width / 2.0;
-                        const int texture_y = gameState.playerPosition.y - gameState.playerTexture.height / 2.0 - bullet.texture.height / 2.0;
+                        const int texture_y = gameState.playerPosition.y - atlas.playerTexture.height / 2.0 - bullet.texture.height / 2.0;
                         DrawTexture(bullet.texture, texture_x, texture_y, WHITE);
                     }
                 }
@@ -386,14 +360,14 @@ int main() {
 						}
                         bullet->position.y -= bullet->velocity * GetFrameTime();
                         const int texture_x = bullet->position.x - bullet->texture.width / 2.0;
-                        const int texture_y = bullet->position.y - gameState.playerTexture.height / 2.0 - bullet->texture.height / 2.0;
+                        const int texture_y = bullet->position.y - atlas.playerTexture.height / 2.0 - bullet->texture.height / 2.0;
                         DrawTexture(bullet->texture, texture_x, texture_y, WHITE);
                     }
                 }
                 // Update Player
                 Rectangle destination = {texture_x, texture_y, 32, 62}; // origin in coordinates and scale
                 Vector2 origin = { 0 }; // so it draws from top left of image
-                DrawSpriteAnimationPro(gameState.playerAnimation, destination, origin, 0, WHITE);
+                DrawSpriteAnimationPro(atlas.playerAnimation, destination, origin, 0, WHITE);
 
                 // Spawn Enemies
                 {
@@ -410,17 +384,19 @@ int main() {
                             .size = size,
                         };
                         int whichAsteroid = GetRandomValue(1,10);
+                        Texture2D enemyTexture;
                         if (whichAsteroid < 4)
                         {
-                            sprintf(enemy.textureFile, "%s", "assets/asteroid2.png");
+                            enemyTexture = atlas.enemyTexture2;
+                            // sprintf(enemy.textureFile, "%s", "assets/asteroid2.png");
                             enemy.type = 2;
                         } 
                         else 
                         {
-                            sprintf(enemy.textureFile, "%s", "assets/asteroid1.png");
+                            enemyTexture = atlas.enemyTexture1;
                             enemy.type = 1;
                         }
-                        enemy.texture = LoadTexture(enemy.textureFile);
+                        enemy.texture = enemyTexture;
                         enemy.position.y -= enemy.texture.height; // to make them come into screen smoothly
                         gameState.spawnTime = 0;
                         gameState.enemies[gameState.enemyCount++] = enemy;
@@ -497,10 +473,9 @@ int main() {
                 {
                     for (int i = 1; i <= gameState.playerHealth; i++)
                     {
-                        Texture2D texture = LoadTexture("assets/heart.png");
                         const int texture_x = i * 16.0;
-                        const int texture_y = texture.height;
-                        DrawTexture(texture, texture_x, texture_y, WHITE);
+                        const int texture_y = atlas.healthTexture.height;
+                        DrawTexture(atlas.healthTexture, texture_x, texture_y, WHITE);
                     }
                 }
                 // Update Score
@@ -532,15 +507,12 @@ int main() {
                 ClearBackground(BLACK);
                 draw_text_centered(font, "LEVEL UP!", (Vector2){SCREEN_WIDTH/2.0f, SCREEN_HEIGHT/2.0f - 35.0}, 40, fontSpacing, WHITE);
                 draw_text_centered(font, "CHOOSE UPGRADE", (Vector2){SCREEN_WIDTH/2.0f, SCREEN_HEIGHT/2.0f - 80.0}, 40, fontSpacing, WHITE);
-                Texture2D upgradeMultishotTexture = LoadTexture("assets/upgradeMultiShot.png");
-                Texture2D upgradeDamage = LoadTexture("assets/upgradeDamage.png");
-                Texture2D upgradeFireRate = LoadTexture("assets/upgradeFireRate.png");
-                const int texture_x = upgradeMultishotTexture.width;
-                const int texture_y = upgradeMultishotTexture.height;
+                const int texture_x = atlas.upgradeMultishotTexture.width;
+                const int texture_y = atlas.upgradeMultishotTexture.height;
                 const int spacing_x = 10;
-                DrawTexture(upgradeMultishotTexture, SCREEN_WIDTH/2.0f - texture_x/2.0f, SCREEN_HEIGHT/2.0f - texture_y/2.0f + 30.0, WHITE);
-                DrawTexture(upgradeDamage, SCREEN_WIDTH/2.0f - texture_x/2.0f - texture_x - spacing_x, SCREEN_HEIGHT/2.0f - texture_y/2.0f + 30.0, WHITE);
-                DrawTexture(upgradeFireRate,SCREEN_WIDTH/2.0f - texture_x/2.0f + texture_x + spacing_x, SCREEN_HEIGHT/2.0f - texture_y/2.0f + 30.0, WHITE);
+                DrawTexture(atlas.upgradeMultishotTexture, SCREEN_WIDTH/2.0f - texture_x/2.0f, SCREEN_HEIGHT/2.0f - texture_y/2.0f + 30.0, WHITE);
+                DrawTexture(atlas.upgradeDamageTexture, SCREEN_WIDTH/2.0f - texture_x/2.0f - texture_x - spacing_x, SCREEN_HEIGHT/2.0f - texture_y/2.0f + 30.0, WHITE);
+                DrawTexture(atlas.upgradeFirerateTexture, SCREEN_WIDTH/2.0f - texture_x/2.0f + texture_x + spacing_x, SCREEN_HEIGHT/2.0f - texture_y/2.0f + 30.0, WHITE);
                 if (IsKeyPressed(KEY_ENTER)) {                    
                     gameState.playerMultishot = true;
                     gameState.state = STATE_RUNNING;
@@ -557,9 +529,10 @@ int main() {
                 draw_text_centered(font, "<Press enter to try again>", (Vector2){SCREEN_WIDTH/2.0f, SCREEN_HEIGHT/2.0f + 60}, 20, fontSpacing, WHITE);
                 if (IsKeyPressed(KEY_ENTER)) {
                     gameState = defaultGameState;
+                    initTextureAtlas(&atlas);
                     Texture2D playerTexture = LoadTexture("assets/rocketNew.png");
-                    gameState.playerTexture = playerTexture;
-                    gameState.playerAnimation = createSpriteAnimation(playerTexture, 10, (Rectangle[]) {
+                    atlas.playerTexture = playerTexture;
+                    atlas.playerAnimation = createSpriteAnimation(playerTexture, 10, (Rectangle[]) {
                         (Rectangle){0,0,32,62},   (Rectangle){32,0,32,62},  (Rectangle){64,0,32,62},  (Rectangle){96,0,32,62},
                         (Rectangle){128,0,32,62}, (Rectangle){160,0,32,62}, (Rectangle){192,0,32,62}, (Rectangle){224,0,32,62},
                         (Rectangle){256,0,32,62}, (Rectangle){288,0,32,62}, (Rectangle){320,0,32,62}, (Rectangle){352,0,32,62},
@@ -580,10 +553,11 @@ int main() {
                 break;
             }
         }
+        DrawFPS(10, 40);
         EndDrawing();
     }
 
-    FreeSpriteAnimation(gameState.playerAnimation);
+    FreeSpriteAnimation(atlas.playerAnimation);
     UnloadFont(font);
     CloseWindow();
     return 0;
